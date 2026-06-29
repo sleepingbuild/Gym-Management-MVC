0973830f755513f463c62fc44addb334e7033175
sleepingbuild <phipham690@gmail.com>
2026-06-29 22:30:35 +0800

    feat(trainer): implement trainer management UI #12
    - Add Trainer Index view with cards and statistics
    - Add Trainer Create view
    - Add Trainer Edit view
    - Add Trainer Details view
    - Add status toggle functionality
    - Add responsive design with fitn
    
    ess style

---
+++ GYM-MANAGEMENT-SYSTEM/Views/Trainer/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GYM-MANAGEMENT-SYSTEM.Views.Trainer
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
